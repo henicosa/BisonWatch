56b5c652a82333dfed087cd0315d87eb716ddf99
viji5369 <ludwig.david.lorenz@uni-weimar.de>
2021-09-02 18:29:34 +0200

    precise project structure + dsv parse bison data

---
+++ code/parallel_sets/main.js
@@ -0,0 +1,21 @@
+import * as d3 from "d3";
+import { loadBisonDataset } from "../bison";
+
+
+// Importe für alte Visualisierungen
+/*import { discretize } from "./vislibs/discretize";
+import { parallelsets } from "./vislibs/parallelsets";
+import { mosaicplot, sliceAndDice } from "./vislibs/mosaicplot";
+import { createHierarchy } from "./vislibs/hierarchy";
+import marked from "marked";
+import whatwhyhow from "./whatwhyhow.md";
+import { parallelcoordinates } from "./parallelcoordinates";*/
+
+
+// Laden der Klimadaten
+loadBisonDataset().then((bisond) => {
+  console.log(bisond)
+});
+
+
+
