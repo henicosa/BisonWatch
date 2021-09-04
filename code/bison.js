6e53b25727405a203a9f06fe37df97757f47ee78
Sophie <[sophiegrimme@gmx.de]>
2021-09-04 11:01:08 +0200

    fix Bisondata parse

--- code/bison.js
+++ code/bison.js
@@ -5,7 +5,7 @@ export async function loadBisonDataset() {
     courseTitle: item.Veranstaltungstitel,
     internalLink: item.Bisonlink,
     courseType: item.Veranstaltungsart,
-    sws: +item.sws, // transform string to number
+    sws: + item.SWS, // transform string to number
     language: item.Sprache, 
     day: item.Tag,
   }));
