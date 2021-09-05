6415eac726f37240861491abe5e7b02d45e1a1d7
viji5369 <ludwig.david.lorenz@uni-weimar.de>
2021-09-05 21:17:06 +0200

    add crawler + site navigation + site text

--- code/lecturer_network/main.js
+++ code/lecturer_network/main.js
@@ -37,107 +37,103 @@ loadBisonDataset().then((bisond) => {
   });
   console.log(lecturers)
 
-  var data = d3.json("https://gist.githubusercontent.com/mbostock/4062045/raw/5916d145c8c048a6e3086915a6be464467391c62/miserables.json")
-    .then((data) =>
-    {
-      var height = 600
-      var width = 1000
-
-      console.log(data)
-
-      const scale = d3.scaleLinear()
-        .domain([-0, 16, 50])
-        .range(["grey", "blue", "red"]);
-      var color =  d => scale(d.group);
-
-      var mdata = {nodes: [], links: []};
-      lecturers.forEach((value, lecturer) => {
-        mdata.nodes.push({id: lecturer, group: value.courses.size})
-        value.colecturers.forEach((lecture_num, colecturer) => {
-          if (lecturer < colecturer) {
-            mdata.links.push({source: lecturer,target: colecturer, value: lecture_num})
-          }
-        })
-      })
-      console.log(mdata)
-      // Interaction
-      data = mdata
-
-      var drag = simulation => {
   
-        function dragstarted(event) {
-          if (!event.active) simulation.alphaTarget(0.3).restart();
-          event.subject.fx = event.subject.x;
-          event.subject.fy = event.subject.y;
+    var height = 800
+    var width = 1000
+
+
+    const scale = d3.scaleLinear()
+      .domain([-0, 16, 50])
+      .range(["grey", "blue", "red"]);
+    var color =  d => scale(d.group);
+
+    var mdata = {nodes: [], links: []};
+    lecturers.forEach((value, lecturer) => {
+      mdata.nodes.push({id: lecturer, group: value.courses.size})
+      value.colecturers.forEach((lecture_num, colecturer) => {
+        if (lecturer < colecturer) {
+          mdata.links.push({source: lecturer,target: colecturer, value: lecture_num})
         }
-        
-        function dragged(event) {
-          event.subject.fx = event.x;
-          event.subject.fy = event.y;
-        }
-        
-        function dragended(event) {
-          if (!event.active) simulation.alphaTarget(0);
-          event.subject.fx = null;
-          event.subject.fy = null;
-        }
-        
-        return d3.drag()
-            .on("start", dragstarted)
-            .on("drag", dragged)
-            .on("end", dragended);
+      })
+    })
+    console.log(mdata)
+    // Interaction
+    var data = mdata
+
+    var drag = simulation => {
+
+      function dragstarted(event) {
+        if (!event.active) simulation.alphaTarget(0.3).restart();
+        event.subject.fx = event.subject.x;
+        event.subject.fy = event.subject.y;
       }
-
-      // SVG
-
-      const links = data.links.map(d => Object.create(d));
-      const nodes = data.nodes.map(d => Object.create(d));
-    
-      const simulation = d3.forceSimulation(nodes)
-        .force("link", d3.forceLink(links).id(d => d.id))
-        .force("charge", d3.forceManyBody())
-        .force("x", d3.forceX())
-        .force("y", d3.forceY());
       
-      const svg = d3.select("#lecturer_network")
-          .attr("viewBox", [-width / 2, -height / 2, width, height]);
-    
-      const link = svg.append("g")
-          .attr("stroke", "#999")
-          .attr("stroke-opacity", 0.6)
-        .selectAll("line")
-        .data(links)
-        .join("line")
-          .attr("stroke-width", d => Math.sqrt(d.value));
-    
-      const node = svg.append("g")
-          .attr("stroke", "#fff")
-          .attr("stroke-width", 1.5)
-        .selectAll("circle")
-        .data(nodes)
-        .join("circle")
-          .attr("r", d => 5 + parseInt(Math.log(d.group)/ Math.log(1.5)))
-          .attr("fill", color)
-          .call(drag(simulation));
-    
-      node.append("title")
-          .text(d => d.id);
-    
-      simulation.on("tick", () => {
-        link
-            .attr("x1", d => d.source.x)
-            .attr("y1", d => d.source.y)
-            .attr("x2", d => d.target.x)
-            .attr("y2", d => d.target.y);
-    
-        node
-            .attr("cx", d => d.x)
-            .attr("cy", d => d.y);
-      });
-    
-      //invalidation.then(() => simulation.stop());
+      function dragged(event) {
+        event.subject.fx = event.x;
+        event.subject.fy = event.y;
+      }
+      
+      function dragended(event) {
+        if (!event.active) simulation.alphaTarget(0);
+        event.subject.fx = null;
+        event.subject.fy = null;
+      }
+      
+      return d3.drag()
+          .on("start", dragstarted)
+          .on("drag", dragged)
+          .on("end", dragended);
+    }
 
+    // SVG
+
+    const links = data.links.map(d => Object.create(d));
+    const nodes = data.nodes.map(d => Object.create(d));
+  
+    const simulation = d3.forceSimulation(nodes)
+      .force("link", d3.forceLink(links).id(d => d.id))
+      .force("charge", d3.forceManyBody())
+      .force("x", d3.forceX())
+      .force("y", d3.forceY());
+    
+    const svg = d3.select("#lecturer_network")
+        .attr("viewBox", [-width / 2, -height / 2, width, height]);
+  
+    const link = svg.append("g")
+        .attr("stroke", "#999")
+        .attr("stroke-opacity", 0.6)
+      .selectAll("line")
+      .data(links)
+      .join("line")
+        .attr("stroke-width", d => Math.sqrt(d.value));
+  
+    const node = svg.append("g")
+        .attr("stroke", "#fff")
+        .attr("stroke-width", 1.5)
+      .selectAll("circle")
+      .data(nodes)
+      .join("circle")
+        .attr("r", d => 5 + parseInt(Math.log(d.group)/ Math.log(1.5)))
+        .attr("fill", color)
+        .call(drag(simulation));
+  
+    node.append("title")
+        .text(d => d.id);
+  
+    simulation.on("tick", () => {
+      link
+          .attr("x1", d => d.source.x)
+          .attr("y1", d => d.source.y)
+          .attr("x2", d => d.target.x)
+          .attr("y2", d => d.target.y);
+  
+      node
+          .attr("cx", d => d.x)
+          .attr("cy", d => d.y);
     });
+  
+    //invalidation.then(() => simulation.stop());
 
-});
+  });
+
 
