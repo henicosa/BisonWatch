95eac691e522bebf2c504bbfdd0c08250ca31e01
sophiegri <sophiegrimme@gmx.de>
2021-09-07 14:53:40 +0200

    Finish Titanic Parallel Set

--- code/titanic.js
+++ code/titanic.js
@@ -2,10 +2,10 @@ import { csv, isoParse } from "d3";
 
 export async function loadTitanicDataset() {
   return await csv("/data/titanic.csv", (item) => ({
-    survived: item.Survived,
-    sex: item.Sex,
-    age: item.Age, 
-    class: item.Class, 
-    value: +item.Value
+    Survived: item.Survived,
+    Sex: item.Sex,
+    Age: item.Age, 
+    Class: item.Class, 
+    value: +item.value
   }));
 }
