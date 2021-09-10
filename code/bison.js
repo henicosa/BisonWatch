c88d60b72ff8c0c9002314ff11c4bc7015b58c10
viji5369 <ludwig.david.lorenz@uni-weimar.de>
2021-09-10 10:56:51 +0200

    new data + markdown import

--- code/bison.js
+++ code/bison.js
@@ -1,7 +1,7 @@
 import { dsv, isoParse } from "d3";
 
 export async function loadBisonDataset() {
-  return await dsv("\t", "/data/bisondata20201.csv", (item) => ({
+  return await dsv("\t", "/data/bisondata.csv", (item) => ({
     courseTitle: item.Veranstaltungstitel,
     internalLink: item.Bisonlink,
     courseType: item.Veranstaltungsart,
