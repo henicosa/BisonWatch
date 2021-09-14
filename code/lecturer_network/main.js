d4bdba3f3fada0d4b180423ba8b22a30905a2df9
viji5369 <ludwig.david.lorenz@uni-weimar.de>
2021-09-14 19:54:15 +0200

    fix lecturer network selection

--- code/lecturer_network/main.js
+++ code/lecturer_network/main.js
@@ -102,243 +102,248 @@ loadBisonDataset(dataset).then((bisond) => {
   });
 
   
-    var height = 1000
-    var width = 1000
+  var height = 1000
+  var width = 1000
 
-    var colors = new Map().set("Fakultät Architektur und Urbanistik", "#009BB4")
-                  .set("Fakultät Bauingenieurwesen", "#F39100")
-                  .set("Fakultät Kunst und Gestaltung", "#94C11C")
-                  .set("Fakultät Medien", "#006B94")
-                  .set("Sonstiges", "grey")
+  var colors = new Map().set("Fakultät Architektur und Urbanistik", "#009BB4")
+                .set("Fakultät Bauingenieurwesen", "#F39100")
+                .set("Fakultät Kunst und Gestaltung", "#94C11C")
+                .set("Fakultät Medien", "#006B94")
+                .set("Sonstiges", "grey")
 
-    const scale = d3.scaleLinear()
-      .domain([0, 16, 50])
-      .range(["grey", "blue", "red"]);
+  const scale = d3.scaleLinear()
+    .domain([0, 16, 50])
+    .range(["grey", "blue", "red"]);
 
-    var mdata = {nodes: [], links: []};
-    lecturers.forEach((value, lecturer_name) => {
-      mdata.nodes.push({id: lecturer_name, group: value.courses.size, faculty: value.faculty})
-      value.colecturers.forEach((lecture_num, colecturer_name) => {
-        if (lecturer_name < colecturer_name) {
-          mdata.links.push({source: lecturer_name,target: colecturer_name, value: lecture_num})
-        }
-      })
+  var mdata = {nodes: [], links: []};
+  lecturers.forEach((value, lecturer_name) => {
+    mdata.nodes.push({id: lecturer_name, group: value.courses.size, faculty: value.faculty})
+    value.colecturers.forEach((lecture_num, colecturer_name) => {
+      if (lecturer_name < colecturer_name) {
+        mdata.links.push({source: lecturer_name,target: colecturer_name, value: lecture_num})
+      }
     })
+  })
 
-    // Interaction
-    var data = mdata
+  // Interaction
+  var data = mdata
 
-    var drag = simulation => {
+  var drag = simulation => {
 
-      function dragstarted(event) {
-        if (!event.active) simulation.alphaTarget(0.3).restart();
-        event.subject.fx = event.subject.x;
-        event.subject.fy = event.subject.y;
-      }
-      
-      function dragged(event) {
-        event.subject.fx = event.x;
-        event.subject.fy = event.y;
-      }
-      
-      function dragended(event) {
-        if (!event.active) simulation.alphaTarget(0);
-        event.subject.fx = null;
-        event.subject.fy = null;
-      }
-      
-      return d3.drag()
-          .on("start", dragstarted)
-          .on("drag", dragged)
-          .on("end", dragended);
+    function dragstarted(event) {
+      if (!event.active) simulation.alphaTarget(0.3).restart();
+      event.subject.fx = event.subject.x;
+      event.subject.fy = event.subject.y;
     }
-
-    // SVG
-
     
-    const links = data.links.map(d => Object.create(d));
-    const nodes = data.nodes.map(d => Object.create(d));
-
-    function get_force(list, faculty) {
-      var diam = 100
-      var index = list.indexOf(faculty)
-      if (index < list.length-2) {
-        var bogen = 2*Math.PI/(list.length-1)
-        return {x: diam*Math.cos(bogen*index), y: diam*Math.sin(bogen*index)}
-      } else return {x: 0, y: 0}
+    function dragged(event) {
+      event.subject.fx = event.x;
+      event.subject.fy = event.y;
     }
+    
+    function dragended(event) {
+      if (!event.active) simulation.alphaTarget(0);
+      event.subject.fx = null;
+      event.subject.fy = null;
+    }
+    
+    return d3.drag()
+        .on("start", dragstarted)
+        .on("drag", dragged)
+        .on("end", dragended);
+  }
 
-    categories.push("Sonstiges")
-    var force_map = new Map()
-    categories.forEach((d) => {force_map.set(d, get_force(categories, d))})
+  // SVG
 
   
-    const simulation = d3.forceSimulation(nodes)
-      .force("link", d3.forceLink(links).id(d => d.id))
-      .force("charge", d3.forceManyBody())
-      .force("x", d3.forceX(d => force_map.get(d.faculty).x))
-      .force("y", d3.forceY(d => force_map.get(d.faculty).y));
-    
-    const svg = d3.select("#lecturer_network")
-        .attr("viewBox", [-width / 2, -height / 2, width, height])
-    
-    svg.call(d3.zoom()
-      .extent([[0, 0], [width, height]])
-      .scaleExtent([1, 8])
-      .on("zoom", zoomed));
+  const links = data.links.map(d => Object.create(d));
+  const nodes = data.nodes.map(d => Object.create(d));
 
-    var zoom = svg.append("g")
-    
-  
-    function zoomed({transform}) {
-      zoom.attr("transform", transform);
-      zoom.transition()
-    .duration(3750)
-    }
+  function get_force(list, faculty) {
+    var diam = 100
+    var index = list.indexOf(faculty)
+    if (index < list.length-2) {
+      var bogen = 2*Math.PI/(list.length-1)
+      return {x: diam*Math.cos(bogen*index), y: diam*Math.sin(bogen*index)}
+    } else return {x: 0, y: 0}
+  }
 
-    const link = zoom.append("g")
-        .attr("stroke", "#999")
-        .attr("stroke-opacity", 0.6)
-      .selectAll("line")
-      .data(links)
-      .join("line")
-        .attr("stroke-width", d => Math.sqrt(d.value));
+  categories.push("Sonstiges")
+  var force_map = new Map()
+  categories.forEach((d) => {force_map.set(d, get_force(categories, d))})
+
+
+  const simulation = d3.forceSimulation(nodes)
+    .force("link", d3.forceLink(links).id(d => d.id))
+    .force("charge", d3.forceManyBody())
+    .force("x", d3.forceX(d => force_map.get(d.faculty).x))
+    .force("y", d3.forceY(d => force_map.get(d.faculty).y));
   
-    const node = zoom.append("g")
-        .attr("stroke", "#fff")
-        .attr("stroke-width", 1.5)
-      .selectAll("circle")
-      .data(nodes)
-      .join("circle")
-        .attr("r", d => 5 + parseInt(Math.log(d.group)/ Math.log(1.5)))
-        .attr("fill", d => lecturer_selected ? "LightGray" : colors.get(d.faculty))
-        .call(drag(simulation))
-        .on("mouseover", function(d) { 
-          d3.select(this).attr("r", d => 5 + parseInt(Math.log(d.group)/ Math.log(1.5))).style("fill",d => {
-            lecturer_selected = true
-            lecturer_selected_name = d.id
-            redraw()
-            return d.id == lecturer_force_selected_name ? "red" : colors.get(d.faculty)
-          });
-        })                  
-        .on("mouseout", function(d) { 
-          d3.select(this).attr("r", d => 5 + parseInt(Math.log(d.group)/ Math.log(1.5))).style("fill", d => {
-            lecturer_selected = false
-            redraw()
-            return colors.get(lecturer_selected ? "LightGray" : colors.get(d.faculty))
-          });
-        })
-        .on("click", function (e, d) {
-          if (lecturer_force_selected_name == d.id) {
-            remove_selection()
-          } else {
-            make_selection(d.id)
-          }
+  const svg = d3.select("#lecturer_network")
+      .attr("viewBox", [-width / 2, -height / 2, width, height])
+  
+  svg.call(d3.zoom()
+    .extent([[0, 0], [width, height]])
+    .scaleExtent([1, 8])
+    .on("zoom", zoomed));
+
+  var zoom = svg.append("g")
+  
+
+  function zoomed({transform}) {
+    zoom.attr("transform", transform);
+    zoom.transition()
+  .duration(3750)
+  }
+
+  const link = zoom.append("g")
+      .attr("stroke", "#999")
+      .attr("stroke-opacity", 0.6)
+    .selectAll("line")
+    .data(links)
+    .join("line")
+      .attr("stroke-width", d => Math.sqrt(d.value));
+
+  const node = zoom.append("g")
+      .attr("stroke", "#fff")
+      .attr("stroke-width", 1.5)
+    .selectAll("circle")
+    .data(nodes)
+    .join("circle")
+      .attr("r", d => 5 + parseInt(Math.log(d.group)/ Math.log(1.5)))
+      .attr("fill", d => lecturer_selected ? "LightGray" : colors.get(d.faculty))
+      .call(drag(simulation))
+      .on("mouseover", function(e) { 
+        d3.select(this).attr("r", d => 5 + parseInt(Math.log(d.group)/ Math.log(1.5))).style("fill",d => {
+          lecturer_selected = true
+          lecturer_selected_name = d.id
+          redraw()
+          return (d.id == lecturer_force_selected_name) ? "red" : colors.get(d.faculty)
         });
-  
-    node.append("title")
-        .text(d => d.id);
-  
-    simulation.on("tick", () => {
-      link
-          .attr("x1", d => d.source.x)
-          .attr("y1", d => d.source.y)
-          .attr("x2", d => d.target.x)
-          .attr("y2", d => d.target.y);
-  
-      node
-          .attr("cx", d => d.x)
-          .attr("cy", d => d.y);
-    });
+      })                  
+      .on("mouseout", function(e) { 
+        d3.select(this).attr("r", d => 5 + parseInt(Math.log(d.group)/ Math.log(1.5))).style("fill", d => {
+          lecturer_selected = false
+          lecturer_selected_name = ""
+          redraw()
+          return colors.get(lecturer_selected ? "LightGray" : colors.get(d.faculty))
+        });
+      })
+      .on("click", function (e, d) {
+        if (lecturer_force_selected_name == d.id) {
+          remove_selection()
+          d3.select(this).style("fill", colors.get(d.faculty))
+        } else {
+          make_selection(d.id)
+          d3.select(this).style("fill", "red")
+        }
+      });
 
-    // fetch url search parameter for lecturer
-    const urlSearchParams = new URLSearchParams(window.location.search);
-    var searchParam = urlSearchParams.get('lecturer');
-    if (searchParam != undefined) {
-      make_selection(searchParam)
-    }
-  
-    // function to redraw the graph on mouseover event
-    function redraw() {
-      zoom.selectAll("circle")
-      .data(nodes)
-      .join("circle")
-        .attr("r", d =>  5 + parseInt(Math.log(d.group)/ Math.log(1.5)))
-        .attr("stroke", d => (lecturer_selected && lecturer_selected_name == d.id) ? 
-        "red" : 
-        "white")
-        .attr("stroke-width", d => (lecturer_selected && lecturer_selected_name == d.id) ? 
-        3 : 
-        2)
-        .attr("fill", d => {
-          if (!force_selection) {
-            if (lecturer_selected && !is_connected(lecturer_selected_name, d.id)) {
-              return "LightGray";
-            } else {
-              return colors.get(d.faculty);
-            }
-          } else {
-            if (d.id == lecturer_force_selected_name) {
-              return "red";
-            } else if (is_connected(lecturer_force_selected_name, d.id)) {
-              return colors.get(d.faculty);
-            } else {
-              return "LightGray";
-            }
-          }
-        }) 
-        .call(drag(simulation))
-    }
+  node.append("title")
+      .text(d => d.id);
 
-    function select_teacher() {
-      zoom.selectAll("circle")
-      .data(nodes)
-      .join("circle")
-        .attr("r", d => 5 + parseInt(Math.log(d.group)/ Math.log(1.5)))
-        .attr("fill", d => (lecturer_selected && !is_connected(lecturer_selected_name, d.id)) ? lecturer_selected_name == d.id ? "red" : "LightGray" : colors.get(d.faculty))
-        .call(drag(simulation))
-    }
+  simulation.on("tick", () => {
+    link
+        .attr("x1", d => d.source.x)
+        .attr("y1", d => d.source.y)
+        .attr("x2", d => d.target.x)
+        .attr("y2", d => d.target.y);
 
-    function remove_selection() {
-      force_selection = false
-      lecturer_selected = false
-      lecturer_selected_name = ""
-      d3.select("#tip").select("div").remove()
-      d3.select("#search_input").property("value", "")
-    }
-    
-
-    function make_selection(input) {
-      var description = d3.select("#description").text("")
-      description.append("c").text("Diese Visualisierung zeigt die Lehrperson ")
-      description.append("strong").text(input)
-      description.append("c").text(" und alle Lehrpersonen mit gemeinsamen Kursen " + (historic_data ? " seit einschließlich WiSe 2019/20."  : "im aktuellen Semeseter."))
-
-      selector_url.searchParams.set("lecturer", input)
-      if (historic_data) selector_url.searchParams.set("historic", "yes")
-      d3.select("#tip").select("div").remove()
-      var tip = description//d3.select("#tip").append("div").text("")
-      tip.append("c").text(" (")
-      tip.append("a").attr("href", selector_url).text("Erfahre mehr über die Veranstaltungen von " + input)
-      tip.append("c").text(")")
-      d3.select("#search_input").property("value", input)
-
-      lecturer_selected_name = input;
-      lecturer_selected = true
-
-      force_selection = true
-      lecturer_force_selected_name = input
-
-      select_teacher()
-    }
-
-    //function to test if two nodes are connected:
-    function is_connected(lecturer_A_name, lecturer_B_name) {
-      return lecturers.get(lecturer_A_name).colecturers.has(lecturer_B_name)
-    }
-
-    //invalidation.then(() => simulation.stop());
-
+    node
+        .attr("cx", d => d.x)
+        .attr("cy", d => d.y);
   });
+
+  // fetch url search parameter for lecturer
+  const urlSearchParams = new URLSearchParams(window.location.search);
+  var searchParam = urlSearchParams.get('lecturer');
+  if (searchParam != undefined) {
+    make_selection(searchParam)
+  }
+
+  // function to redraw the graph on mouseover event
+  function redraw() {
+    zoom.selectAll("circle")
+    .data(nodes)
+    .join("circle")
+      .attr("r", d =>  5 + parseInt(Math.log(d.group)/ Math.log(1.5)))
+      .attr("stroke", d => (lecturer_selected && lecturer_selected_name == d.id) ? 
+      "red" : 
+      "white")
+      .attr("stroke-width", d => (lecturer_selected && lecturer_selected_name == d.id) ? 
+      3 : 
+      2)
+      .attr("fill", d => {
+        if (!force_selection) {
+          if (lecturer_selected && !is_connected(lecturer_selected_name, d.id)) {
+            return "LightGray";
+          } else {
+            return colors.get(d.faculty);
+          }
+        } else {
+          if (d.id == lecturer_force_selected_name) {
+            return "red";
+          } else if (is_connected(lecturer_force_selected_name, d.id)) {
+            return colors.get(d.faculty);
+          } else {
+            return "LightGray";
+          }
+        }
+      }) 
+      .call(drag(simulation))
+  }
+
+  function select_teacher() {
+    zoom.selectAll("circle")
+    .data(nodes)
+    .join("circle")
+      .attr("r", d => 5 + parseInt(Math.log(d.group)/ Math.log(1.5)))
+      .attr("fill", d => (lecturer_selected && !is_connected(lecturer_selected_name, d.id)) ? lecturer_selected_name == d.id ? "red" : "LightGray" : colors.get(d.faculty))
+      .call(drag(simulation))
+  }
+
+  function remove_selection() {
+    force_selection = false
+    lecturer_selected = false
+    lecturer_selected_name = ""
+    lecturer_force_selected_name = ""
+    d3.select("#tip").select("div").remove()
+    d3.select("#search_input").property("value", "")
+    d3.select("#description").text("Diese Visualisierung zeigt die Lehrenden der Bauhaus-Universität und ihre gemeinsamen Veranstaltungen" + (historic_data ? " seit einschließlich WiSe 2019/20."  : "im aktuellen Semester."))
+  }
+  
+
+  function make_selection(input) {
+    var description = d3.select("#description").text("")
+    description.append("c").text("Diese Visualisierung zeigt die Lehrperson ")
+    description.append("strong").text(input)
+    description.append("c").text(" und alle Lehrpersonen mit gemeinsamen Kursen " + (historic_data ? " seit einschließlich WiSe 2019/20."  : "im aktuellen Semester."))
+
+    selector_url.searchParams.set("lecturer", input)
+    if (historic_data) selector_url.searchParams.set("historic", "yes")
+    d3.select("#tip").select("div").remove()
+    var tip = description//d3.select("#tip").append("div").text("")
+    tip.append("c").text(" (")
+    tip.append("a").attr("href", selector_url).text("Erfahre mehr über die Veranstaltungen von " + input)
+    tip.append("c").text(")")
+    d3.select("#search_input").property("value", input)
+
+    lecturer_selected_name = input;
+    lecturer_selected = true
+
+    force_selection = true
+    lecturer_force_selected_name = input
+
+    select_teacher()
+  }
+
+  //function to test if two nodes are connected:
+  function is_connected(lecturer_A_name, lecturer_B_name) {
+    return lecturers.get(lecturer_A_name).colecturers.has(lecturer_B_name)
+  }
+
+  //invalidation.then(() => simulation.stop());
+
+});
 
 
