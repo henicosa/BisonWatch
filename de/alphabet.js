54a7fe4b4c43c9d726319a5c52c43ad13dbfac29
viji5369 <ludwig.david.lorenz@uni-weimar.de>
2022-01-30 15:49:43 +0100

    removed orphaned dependencies

--- de/alphabet.js
+++
@@ -1,8 +0,0 @@
-import { csv, isoParse } from "d3";
-
-export async function loadAlphabetDataset() {
-  return await csv("/data/alphabet.csv", (item) => ({
-    letter: item.letter,
-    frequency: +item.frequency
-  }));
-}
